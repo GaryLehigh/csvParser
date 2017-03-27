7eb76ae29510e9761d1f56ff97ed6ff52becc45f
GaryLehigh <xig514@lehigh.edu>
2017-03-27 20:01:00 +0800

    Create ChineseCharacters.py

---
+++ ChineseCharacters.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 
+
+@author: g00377630
+'''
+xiaoqu = [u"小区配额",u"子帧号",u"提前调度标识", u"我爱你", u"上下行配比", u"的数量",  u"错误码", u"二级ePDCCH分配位图0"]
+print (xiaoqu)
+unicodeList =''
+for word in xiaoqu:
+    unicodeList = unicodeList + word
